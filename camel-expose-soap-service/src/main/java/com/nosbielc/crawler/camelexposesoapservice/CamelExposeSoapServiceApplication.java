f15ce206d70b747f3b07f7cb068fa99b5a657a5f
Cleibson Gomes da Silva <cleibson.gomes.da.silva@gmail.com>
2019-04-18 17:53:19 -0300

    camel-expose-soap-service
    camel-expose-soap-service

---
+++ camel-expose-soap-service/src/main/java/com/nosbielc/crawler/camelexposesoapservice/CamelExposeSoapServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nosbielc.crawler.camelexposesoapservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamelExposeSoapServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CamelExposeSoapServiceApplication.class, args);
+    }
+
+}

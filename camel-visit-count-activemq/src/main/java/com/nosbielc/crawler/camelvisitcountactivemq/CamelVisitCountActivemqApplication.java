bdf4800e78f63aaff0e4e4a6ea91be3b9c08df28
Cleibson Gomes da Silva <cleibson.gomes.da.silva@gmail.com>
2019-04-22 18:00:22 -0300

    Add camel-visit-count-activemq
    Add camel-visit-count-activemq

---
+++ camel-visit-count-activemq/src/main/java/com/nosbielc/crawler/camelvisitcountactivemq/CamelVisitCountActivemqApplication.java
@@ -0,0 +1,13 @@
+package com.nosbielc.crawler.camelvisitcountactivemq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamelVisitCountActivemqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CamelVisitCountActivemqApplication.class, args);
+    }
+
+}
---
+++ camel-visit-count-activemq/src/test/java/com/nosbielc/crawler/camelvisitcountactivemq/CamelVisitCountActivemqApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nosbielc.crawler.camelvisitcountactivemq;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CamelVisitCountActivemqApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}

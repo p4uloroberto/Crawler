135e27862dcc0e2e03cac59d854672a7610cbfb0
Cleibson Gomes da Silva <cleibson.gomes.da.silva@gmail.com>
2019-05-03 10:30:41 -0300

    Update Camel-api-db
    Update Camel-api-db

---
+++ camel-api-db/src/main/java/com/nosbielc/crawler/camelapidb/RouterHawtio.java
@@ -0,0 +1,38 @@
+package com.nosbielc.crawler.camelapidb;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RouterHawtio extends RouteBuilder {
+    @Override
+    public void configure() throws Exception {
+        from("direct:Hawtio")
+                .id("Internal::HawtioRouter")
+                .description("Router exemplo para visualização no Hawtio.")
+                .log("Executando Processador 1")
+                .process(new Processor() {
+                    @Override
+                    public void process(Exchange exchange) throws Exception {
+                        System.out.println("Processador 1 executado");
+                    }
+                })
+                .log("Executando Processador 2")
+                .process(new Processor() {
+                    @Override
+                    public void process(Exchange exchange) throws Exception {
+                        System.out.println("Processador 2 executado");
+                    }
+                })
+                .log("Executando Processador 3")
+                .process(new Processor() {
+                    @Override
+                    public void process(Exchange exchange) throws Exception {
+                        System.out.println("Processador 3 executado");
+                    }
+                })
+                .end();
+    }
+}
